fd5fd7ad696221937bd66176ae264f58c07219af
Navneet <navneetjh5@gmail.com>
2020-10-17 18:37:40 +0530

    CREATED: LL Structure created

--- src/main/java/com/cg/datastructures/LinkedList.java
+++ src/main/java/com/cg/datastructures/LinkedList.java
@@ -1,7 +1,40 @@
 package com.cg.datastructures;
 
 public class LinkedList {
-    public static void main(String[] args) {
-        System.out.println("Linked list program");
+
+    private Node head;
+
+    public static class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
     }
+
+    public static void main(String[] args) {
+        Node head = new Node(50);
+        Node second = new Node(30);
+        Node third = new Node(70);
+
+        head.next = second;
+        second.next = third;
+        third.next = null;
+
+        displayList(head);
+
+    }
+
+    public static void displayList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + "-->");
+            current = current.next;
+        }
+        System.out.println(current);
+
+    }
+
 }
